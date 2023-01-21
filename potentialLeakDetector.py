57223954aa12c6bb24ee3cba7fdbcc97d4776568
Josh Thompson <josht200@live.com>
2023-01-21 21:56:10 +0000

    nearity is a go

--- potentialLeakDetector.py
+++ potentialLeakDetector.py
@@ -17,65 +17,49 @@ Take average pressure and minimum temperature
 import matplotlib.pyplot as plt
 import numpy as np
 import GenJohn as gj
-from sklearn import datasets, linear_model, metrics
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import classification_report, confusion_matrix
-
-aMatrix, listOfLists = gj.makeGraphAndTemps(6)
-print("AMatrix", aMatrix)
-print("ListOfLists:", listOfLists) # Coldest temp recorded, largest difference in temp, average pressure (per 12 hours). Each represents
-# listOflists contains data example as a matrix for each edge
-
-# defining feature matrix(X) and response vector(y)
-X = listOfLists[0, ]
-
-#Splitting X and y into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
-
-#create linear regression object
-reg = linear_model.LinearRegression()
-
-#train model using training sets
-reg.fit(X_train, y_train)
-
-#regression coefficients
-print('Coefficients', reg.coef_)
-
-#Variance score: 1 means perfect prediction
-print('Variance score: {}'.format(reg.score(X_test, y_test)))
-
-#plot for residual error
-## set plot style
-plt.style.use('fivethirtyeight')
-
-##plotting residual errors in training data
-plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train, color='blue', s=10, label='Test data')
-
-##plotting line for zero residual error
-plt.hlines(y=0, xmin=0, xmax=50, linewidth=2)
-
-##plotting legend
-plt.legend(loc='upper right')
-
-##plot title
-plt.title('Residual errors')
-
-##Show plot
-plt.show()
-'''
-x = np.arange(10).reshape(-1,1)
-y = np.array([0,0,0,0,1,1,1,1,1,1])
-
-model1 = LinearRegression().fit(x,y)
-
-model2 = LogisticRegression(solver='liblinear', random_state=0)
-model2.fit(x,y)
-
-plt.scatter(x,y)
-plt.show()
-'''
+from sklearn.metrics import mean_squared_error
 
 
+def nearity (nodes):
+   for i in range (0, nodes):
+      aMatrix, listOfLists = gj.makeGraphAndTemps(nodes)
+      # print("AMatrix", aMatrix)
+      # print("ListOfLists:", listOfLists) # Coldest temp recorded, largest difference in temp, average pressure (per 12 hours). Each represents
+      # listOflists contains data example as a matrix for each edge
 
+      # defining feature matrix(X) and response vector(y)
+      coldestTemps = listOfLists[0, : , 0]
+      averagePressures = listOfLists[0, : , 2]
+      X = np.column_stack((coldestTemps, averagePressures))
+      y = listOfLists[0, : , 1]
+
+      #Splitting X and y into training and testing sets
+
+      #create linear regression object
+      reg = linear_model.LinearRegression()
+
+      #train model using training sets
+      reg.fit(X, y)
+
+      #regression coefficients
+      #print('Coefficients', reg.coef_)
+
+      #plot for residual error
+      ## set plot style
+      plt.style.use('fivethirtyeight')
+
+      ##plotting residual errors in training data
+      #plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train, color='blue', label='Test data', linewidth=3)
+
+      ##plotting line for zero residual error
+      #plt.hlines(y=0, xmin=0, xmax=50, linewidth=2)
+
+      ## y-intercept
+      print('y-intercept for node', i, "is", reg.intercept_)
+
+
+nearity(10)
