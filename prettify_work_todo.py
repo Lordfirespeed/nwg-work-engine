523322988906347c5cd3a7fc0ad9829c052c7c1f
Lordfirespeed <joe@joeclack.me.uk>
2023-01-21 21:01:00 +0000

    fix typo; prettify method for showing a table of things to do

--- prettify_work_todo.py
+++ prettify_work_todo.py
@@ -1,5 +1,43 @@
+from find_leaks import Leak
 from IPython.display import display, HTML
 
 
-def hello_world():
-    display(HTML("<h1>Hello, world!</h1>"))
+def create_leak_row(leak: Leak) -> str:
+    return f"""
+    <tr class="danger">
+        <td>Currently leaking {leak.flow_loss} flow per second; {round(leak.flow_loss_coefficient, 2) * 100}% of junction flow</td>
+        <td>Junction {leak.from_node}</td>
+        <td>Highest</td>
+    </tr>
+    """
+
+
+def display_work_todo(leaks: [Leak], predictions) -> None:
+
+    leak_rows = "\n".join([create_leak_row(leak) for leak in leaks])
+
+    display(HTML(
+        f"""
+        <!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Fault</th>
+                            <th scope="col">Location</th>
+                            <th scope="col">Priority</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {leak_rows}
+                    </tbody>
+                </table>
+            </body>
+        </html>
+        """
+    ))
